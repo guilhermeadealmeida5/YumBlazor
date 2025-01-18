2c2af43eeaa36094a6eb6cc7a24484e24cc0ae7f
Guilherme Almeida <guilherme.almeida@brasoftware.com.br>
2025-01-18 18:54:11 -0300

    Learn blazor - toastr notification implemented

---
+++ YumBlazor/Data/Category.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace YumBlazor.Data
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Por favor, forneça o nome")]
+        public string Name { get; set; }
+    }
+}

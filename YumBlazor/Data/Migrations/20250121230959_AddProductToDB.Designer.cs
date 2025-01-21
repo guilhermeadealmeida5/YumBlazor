f85bdd8166523658306b2729fe0feb749a4c7b31
Guilherme Almeida <guilherme.almeida@brasoftware.com.br>
2025-01-21 20:37:01 -0300

    Criando CRUD de produtos

--- YumBlazor/Data/Migrations/20250121230959_AddProductToDB.Designer.cs
+++ YumBlazor/Data/Migrations/20250121230959_AddProductToDB.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using YumBlazor.Data;
 
@@ -11,9 +12,11 @@ using YumBlazor.Data;
 namespace YumBlazor.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250121230959_AddProductToDB")]
+    partial class AddProductToDB
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -269,6 +272,7 @@ namespace YumBlazor.Migrations
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("ImageUrl")
+                        .IsRequired()
                         .HasColumnType("nvarchar(max)");
 
                     b.Property<string>("Name")
